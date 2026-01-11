b5cf480853948258803bbd8dce9839b776ce6085
anilsadioglu <anilsadioglu@radiantcat.studio>
2026-01-11 17:25:39 +0300

    Add: Item ID converter has created.

---
+++ Assets/Scripts/Gameplay/Data/Utils/ItemConverterUtils.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Gameplay.Data.Interfaces;
+using Gameplay.Data.Inventory;
+
+namespace Gameplay.Data.Utils
+{
+    public static partial class ItemConverterUtils
+    {
+        private static Dictionary<(int, string), InventoryItemID> inventoryItemIDs = new();
+        public static InventoryItemID ToID(this InventoryItemType itemType, string hash = default)
+        {
+            if (inventoryItemIDs.TryGetValue(((int)itemType, hash), out InventoryItemID id))
+            {
+                return id;
+            }
+            InventoryItemID target = new InventoryItemID(itemType, hash);
+            inventoryItemIDs.Add(((int)itemType, hash), target);
+            return target;
+        }
+    
+        public static InventoryItemID ToID<TEnum>(this InventoryItemType itemType, TEnum enumValue) where TEnum : Enum
+        {
+            return itemType.ToID(enumValue.ToString());
+        }
+    }
+}
