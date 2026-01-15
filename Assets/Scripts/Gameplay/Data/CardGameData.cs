11a4285f17f39df2015fb9434430261f276d00e9
anilsadioglu <anilsadioglu@radiantcat.studio>
2026-01-15 03:24:06 +0300

    Update: Controlling wheel types and bombs added for card game data.

--- Assets/Scripts/Gameplay/Data/CardGameData.cs
+++ Assets/Scripts/Gameplay/Data/CardGameData.cs
@@ -4,28 +4,48 @@ using Gameplay.Data.Rewards;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 namespace Gameplay.Data
 {
     [CreateAssetMenu(fileName = "card_game_data", menuName = "CardGame/Data", order = 0)]
     public class CardGameData : ScriptableObject
     {
         //Shows the error box on inspector in case of silver and gold steps include bomb. It is an assistant on building data.
-        [InfoBox("Elements that are multiples of 5 cannot contain bombs. Check the elements.", InfoMessageType.Error, VisibleIf = nameof(ShowErrorBox))]
-        [ListDrawerSettings(ShowFoldout = false, DraggableItems = false, ShowIndexLabels = true)]
-        [SerializeField] public List<Step> StepList = new List<Step>();
+        [InfoBox("Elements that are multiples of 5 cannot contain bombs. Check the elements.", InfoMessageType.Error, VisibleIf = nameof(ValidateSteps))]
+        [ListDrawerSettings(ShowFoldout = false, DraggableItems = false, ShowIndexLabels = true, AddCopiesLastElement = true)]
+        public List<Step> StepList = new List<Step>();
 
-        [SerializeField] public List<WheelImageInfo> WheelImageInfos = new List<WheelImageInfo>();
+        public List<WheelDisplayInfo> WheelImageInfos = new List<WheelDisplayInfo>();
+
+        public WheelDisplayInfo GetWheelInfo(WheelType wheelType)
+        {
+            return WheelImageInfos.Find(target => target.WheelType == wheelType);
+        }
 
 #if UNITY_EDITOR
-        private bool ShowErrorBox()
+        private bool ValidateSteps()
         {
-            for (int i = 0; i < StepList.Count; i += 5)
+            for (int i = 0; i <= StepList.Count; i += 5)
             {
                 var index = i == 0 ? i : i - 1;
+
+                //Sets wheel type on step list validated.
+                if (i == 0)
+                {
+                    StepList[index].WheelType = WheelType.Silver;
+                }
+                else if (i % 30 == 0 )
+                {
+                    StepList[index].WheelType = WheelType.Gold;
+                }
+                else if (i % 5 == 0)
+                {
+                    StepList[index].WheelType = WheelType.Silver;
+                }
+                else
+                {
+                    Debug.Log(index + ". items wheel type is Bronze");
+                    StepList[index].WheelType = WheelType.Bronze;
+                }
 
                 foreach (var reward in StepList[index].Rewards)
                 {
@@ -47,6 +67,8 @@ namespace Gameplay.Data
         [InfoBox("This array's element count must be 8.", InfoMessageType.Warning)]
         [ValidateInput(nameof(ValidateRewards))]
         public StepRewardInfo[] Rewards = new StepRewardInfo[8];
+
+        public WheelType WheelType;
 
 #if UNITY_EDITOR
         private bool ValidateRewards(StepRewardInfo[] value)
@@ -76,11 +98,12 @@ namespace Gameplay.Data
     }
 
     [System.Serializable]
-    public struct WheelImageInfo
+    public struct WheelDisplayInfo
     {
         public Sprite WheelImage;
         public Sprite CursorImage;
         public WheelType WheelType;
+        public Color TextColor;
     }
 
     public enum WheelType
